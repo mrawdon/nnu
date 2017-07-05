1a35accb84e54ea2048d2a9b3ca38286c3654388
Tal Rasha <talrasha007@gmail.com>
2017-07-06 03:11:53 +0800

    replace indent to space.

--- examples/main.cpp
+++ examples/main.cpp
@@ -2,40 +2,40 @@
 
 class SampleClass : public nnu::ClassWrapper<SampleClass> {
 public:
-	static const char * const CLASS_NAME;
+    static const char * const CLASS_NAME;
 
-	static NAN_METHOD(ctor) {
-		SampleClass *sc = new SampleClass;
-		sc->Wrap(info.This());
+    static NAN_METHOD(ctor) {
+        SampleClass *sc = new SampleClass;
+        sc->Wrap(info.This());
 
-		info.GetReturnValue().Set(info.This());
-	}
+        info.GetReturnValue().Set(info.This());
+    }
 
-	static void setupMember(v8::Local<v8::FunctionTemplate>& tpl) {
-		Nan::SetPrototypeMethod(tpl, "getVal", wrapFunction<&SampleClass::getVal>);
-		Nan::SetPrototypeMethod(tpl, "incVal", wrapFunction<&SampleClass::incVal>);
-	}
-
-private:
-	SampleClass() : _val(0) { }
-
-	NAN_METHOD(getVal) {
-		info.GetReturnValue().Set(Nan::New(_val));
-	}
-
-	NAN_METHOD(incVal) {
-		_val++; 
-		info.GetReturnValue().Set(info.This());
-	}
+    static void setupMember(v8::Local<v8::FunctionTemplate>& tpl) {
+        Nan::SetPrototypeMethod(tpl, "getVal", wrapFunction<&SampleClass::getVal>);
+        Nan::SetPrototypeMethod(tpl, "incVal", wrapFunction<&SampleClass::incVal>);
+    }
 
 private:
-	int _val;
+    SampleClass() : _val(0) { }
+
+    NAN_METHOD(getVal) {
+        info.GetReturnValue().Set(Nan::New(_val));
+    }
+
+    NAN_METHOD(incVal) {
+        _val++; 
+        info.GetReturnValue().Set(info.This());
+    }
+
+private:
+    int _val;
 };
 
 const char * const SampleClass::CLASS_NAME = "SampleClass";
 
 NAN_MODULE_INIT(InitAll) {
-	SampleClass::setup(target);
+    SampleClass::setup(target);
 }
 
 NODE_MODULE(nnu_example, InitAll);
